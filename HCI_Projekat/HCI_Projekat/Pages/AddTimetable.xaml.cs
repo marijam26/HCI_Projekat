a9d7b5ba8000bcdb037facc2326a36a786ecf71a
Ivana Jankovic <ivanaj0610@gmail.com>
2022-06-08 20:39:34 +0200

    Implementirano dodavanje novog reda voznje

--- HCI_Projekat/HCI_Projekat/Pages/AddTimetable.xaml.cs
+++ HCI_Projekat/HCI_Projekat/Pages/AddTimetable.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using HCI_Projekat.Model;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HCI_Projekat.Pages
 {
@@ -23,10 +23,164 @@ namespace HCI_Projekat.Pages
     {
         public Data dataBase { get; set; }
 
+        public Timetable NewTimetable { get; set; }
+
         public AddTimetable(Data dataBase)
         {
             InitializeComponent();
             this.dataBase = dataBase;
+            fromPlace.ItemsSource = getStations();
+            toPlace.ItemsSource = getStations();
+            train.ItemsSource = getTrains();
+        }
+
+        private IEnumerable getStations()
+        {
+            List<string> stations = new List<string>();
+            foreach (TrainLine line in dataBase.trainLines)
+            {
+                foreach (Station s in line.stations)
+                {
+                    if (!stations.Contains(s.name.ToLower()))
+                    {
+                        stations.Add(s.name.ToLower());
+                    }
+                }
+            }
+            return stations;
+        }
+
+        private IEnumerable getTrains()
+        {
+            List<string> trains = new List<string>();
+            foreach (Train t in dataBase.trains)
+            {
+                if (!trains.Contains(t.name.ToLower()))
+                {
+                    trains.Add(t.name.ToLower());
+                }
+            }
+            return trains;
+        }
+
+        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = (MainWindow)Window.GetWindow(this);
+            TimetableCRUD t = new TimetableCRUD(this.dataBase);
+            window.Content = t;
+        }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            if (!checkInput())
+            {
+                return;
+            }
+            int id = this.dataBase.timetables.Max(x => x.id) + 1;
+            Boolean isWeekday = (bool)rb_weekday.IsChecked ? true : false;
+
+            // find line
+            String from = fromPlace.Text.Trim().ToLower();
+            String to = toPlace.Text.Trim().ToLower();
+
+            TrainLine line = null;
+            foreach(TrainLine l in dataBase.trainLines)
+            {
+                if (l.stations[0].name.ToLower() == from && l.stations[l.stations.Count-1].name.ToLower() == to)
+                {
+                    line = l; 
+                    break;
+                }
+            }
+            if(line == null)
+            {
+                MessageBox.Show("There is no line with chosen stations.", "Invalid", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            Train t = null;
+            foreach(Train tr in dataBase.trains)
+            {
+                if(tr.name.ToLower() == train.Text.Trim().ToLower())
+                {
+                    t = tr;
+                    break;
+                }
+            }
+
+            if(t == null)
+            {
+                MessageBox.Show("Invalid train name chosen.", "Invalid", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            this.NewTimetable = new Timetable { id = id, start = DateTime.Parse(tb_time.Text.Trim()),  isWeekday = isWeekday, 
+                                                ValidFrom = (DateTime)tb_valid_since.SelectedDate, ValidTo = (DateTime)tb_valid_until.SelectedDate,
+                                                line = line, train = t};     // ... 
+            this.dataBase.timetables.Add(this.NewTimetable);
+
+            MessageBox.Show("Successfully added a new departure time!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            TimetableCRUD tc = new TimetableCRUD(this.dataBase);
+            MainWindow window = (MainWindow)Window.GetWindow(this);
+            window.Content = tc;
+
+        }
+
+        private Boolean checkInput()
+        {
+            String from = fromPlace.Text.Trim().ToLower();
+            if(from == "")
+            {
+                MessageBox.Show("You must choose a start station.", "Invalid", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            String to = toPlace.Text.Trim().ToLower();
+            if (to == "")
+            {
+                MessageBox.Show("You must choose an end station.", "Invalid", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            // provjeri da li postoji ta linija
+
+            String t = train.Text.Trim().ToLower();
+            if (t == "")
+            {
+                MessageBox.Show("You must choose a train.", "Invalid", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            String time = tb_time.Text.Trim();
+            if(time == "")
+            {
+                MessageBox.Show("You must enter a departure time.", "Invalid", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            DateTime datetime = DateTime.Parse(time);
+
+            DateTime? validSince = tb_valid_since.SelectedDate;
+            if(validSince == null)
+            {
+                MessageBox.Show("You must choose a validity start date.", "Invalid", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            DateTime? validUntil = tb_valid_until.SelectedDate;
+            if (validUntil == null)
+            {
+                MessageBox.Show("You must choose a validity end date.", "Invalid", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            if(rb_weekday.IsChecked == false && rb_weekend.IsChecked == false)
+            {
+                MessageBox.Show("You must choose between a weekday and a weekend.", "Invalid", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            return true;
         }
     }
 }
